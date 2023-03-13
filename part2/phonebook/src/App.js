d688977bb06cffab2bf3871270e30ebfd8fd36e2
miahro <mikko.ahro@gmail.com>
2023-03-13 14:59:06 +0200

    ex 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,30 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Persons from './components/Persons'
 import Submit_form from './components/Submit_form'
 import Filter_form from './components/Filter_form'
+import axios from 'axios'
 
 const App = () => {
+
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+
   ]) 
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setFilter] = useState('')
+
+
+  useEffect(() => {
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      console.log('promise fulfilled')
+      setPersons(response.data)
+    })
+    }, [])
+  console.log('render', persons.length, 'persons')
+
 
   const addPerson = (event) => {
     event.preventDefault()
